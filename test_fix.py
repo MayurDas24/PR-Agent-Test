26b090a37b0e0f7e0fe99e074348778595b65ca5
MayurDas24 <mayurrdas05@gmail.com>
2025-11-30 00:21:22 +0530

    fix: replace binary test.py with clean UTF-8 text

--- test_fix.py
+++
@@ -1,9 +0,0 @@
-﻿API_KEY = "HARDCODEDSECRET123"
-
-# TODO: optimize this loop
-def compute(x):
-    res = []
-    for i in range(len(x)):
-        for j in range(len(x)):
-            res.append(x[i] * x[j])
-    return res
