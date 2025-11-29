917a97658723b279617ad2cd740be785e8c8dd0a
MayurDas24 <mayurrdas05@gmail.com>
2025-11-30 00:13:57 +0530

    test: add clean UTF-8 test_fix.py to verify diff visibility

---
+++ test_fix.py
@@ -0,0 +1,9 @@
+﻿API_KEY = "HARDCODEDSECRET123"
+
+# TODO: optimize this loop
+def compute(x):
+    res = []
+    for i in range(len(x)):
+        for j in range(len(x)):
+            res.append(x[i] * x[j])
+    return res
